e1880c3d79f9232c0feef60da836f48811932888
Ashwini Manoj <ashwinimanoj@gmail.com>
2019-10-13 00:07:21 +0530

    Replace actions with threading macro

--- quidditch.clj
+++ quidditch.clj
@@ -1,13 +1,16 @@
 (ns basic-clj-project.quidditch)
+
 (defn start-game [team-1 team-2] {team-1 {:score 0 :opponent team-2} team-2 {:score 0 :opponent team-2} :game-over false}) 
 (defn score [game team] (update-in game [team :score] + 10))
 (defn get-opponent [game team] (get-in game [team :opponent]))
 (defn beat [game team] (update-in game [(get-opponent game team) :score] + 10))
 (defn catch-snitch [game team] (assoc (update-in game [team :score] + 150) :game-over true))
 
-(start-game :1 :2)
-(score (start-game :1 :2) :1)
-(beat (start-game :1 :2) :1)
-(catch-snitch (start-game :1 :2) :1)
-(catch-snitch (score (beat(start-game :1 :2) :1) :2) :1)
+(-> (start-game :1 :2)    
+    (score :1)
+    (beat :1)
+    (score :2)
+    (score :1)
+    (beat :1)
+    (catch-snitch :1))
 
