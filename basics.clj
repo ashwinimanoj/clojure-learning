99eb9c91a96bca786fccc7ae9fb819edbcf8d10c
Ashwini Manoj <ashwinimanoj@gmail.com>
2019-11-03 20:55:31 +0530

    Day 1: Types

---
+++ basics.clj
@@ -0,0 +1,37 @@
+; only space matters
+nil
+;; Literals
+
+42
+1.24
+22/7 ; ratio
+0xfff
+6N
+2.45M
+
+(type 42)
+(type 1.24)
+(type 22/7)
+(type 0xfff)
+(type 6N)
+(type 2.45M)
+
+"hello"
+\e
+#"[0-9]+"
+"This is a line
+that can be on multiple lines"
+
+(type "hello")
+(type \e)
+(type #"[0-9]+")
+(type "This is a line
+that can be on multiple lines")
+
+true
+(type true)
+
+:something ; keyword
+(type :something)
+
+
